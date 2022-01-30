990a44d08ee4188465778c4c8c7385f85dbb3403
UrviSinghal28 <90998297+UrviSinghal28@users.noreply.github.com>
2022-01-30 10:33:42 +0530

    Create t-SNE visualization of grain dataset.py

---
+++ Unsupervised Learning in Python/2-Visualization with hierarchical clustering and t-SNE/5-t-SNE visualization of grain dataset/t-SNE visualization of grain dataset.py
@@ -0,0 +1,9 @@
+# Select the 0th feature: xs
+xs = tsne_features[:,0]
+
+# Select the 1st feature: ys
+ys = tsne_features[:,1]
+
+# Scatter plot, coloring by variety_numbers
+plt.scatter(xs,ys,c=variety_numbers)
+plt.show()
